f0dbc39c906e2fc77cd1d42fe9edbc542b331ca4
S2hyeyunS2 <hhyeyun22@gmail.com>
2026-02-20 11:40:15 +0900

    게시물 생성 로직 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { Board } from './board.model';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import type { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private readonly boardsService: BoardsService){}
+  constructor(private readonly boardsService: BoardsService) {}
 
-    @Get('/')
-    getAllBoard(): Board[] {
-        return this.boardsService.getAllBoards();
-    }
+  @Get()
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardsService.createBoard(title, description);
+  }
 }
